6523f1863e407db6af1f7e3a265ee731662b6c31
hasankarim18 <hallysust@gmail.com>
2024-04-21 15:26:07 +0600

    landing page

--- components/SideBar.jsx
+++ components/SideBar.jsx
@@ -3,15 +3,16 @@
 import Link from "next/link";
 
 export default function SideBar({ docs }) {
+  console.log(docs);
   const roots = docs.filter((doc) => doc.parent === null);
   const nonRoots = Object.groupBy(
-    docs.filter((doc) => doc.parent),
-    ({ parent }) => "introduction"
+    docs.filter((doc) => doc.parent != null), // parent level
+    (doc) => doc.parent
   );
 
   // const nonRootFilter = docs.filter((doc) => doc.parent !== null);
 
-  //  console.log({ nonRoots });
+  console.log({ nonRoots });
 
   return (
     <div>
@@ -23,6 +24,7 @@ export default function SideBar({ docs }) {
           <ul role="list" className="border-l border-transparent">
             {roots.map((rootNode) => (
               <li key={rootNode.id} className="relative">
+                {/* root node */}
                 <Link
                   aria-current="page"
                   className="flex justify-between gap-2 py-1 pl-4 pr-3 text-sm text-zinc-900 transition dark:text-white"
@@ -30,23 +32,26 @@ export default function SideBar({ docs }) {
                 >
                   <span className="truncate">{rootNode.title}</span>
                 </Link>
+                {/* subroot node */}
                 {nonRoots[rootNode.id] && (
                   <ul role="list" className="border-l border-transparent">
                     {nonRoots[rootNode.id].map((subRoot) => (
                       <li key={subRoot.id}>
                         <Link
-                          className="flex justify-between gap-2 py-1 pl-7 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
+                          className="flex justify-between gap-2 py-1 pl-8 pr-3 text-sm text-zinc-600 transition hover:text-zinc-900 dark:text-zinc-400 dark:hover:text-white"
                           href={`/docs/${rootNode.id}/${subRoot.id}`}
                         >
                           <span className="truncate">{subRoot.title}</span>
                         </Link>
                       </li>
                     ))}
+                    {/* subroot node ends in below ul */}
                   </ul>
                 )}
-              </li>
+              </li> // this is root node li
             ))}
           </ul>
+          {/* up ul is rootnode ul */}
         </div>
       </ul>
       {/* my approach */}
